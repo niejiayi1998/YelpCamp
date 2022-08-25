9ff4e4ab59a1a7f6c13e0ebc5dfa9f122693c20b
Jiayi Nie <nie.jiay@northeastern.edu>
2022-08-25 14:31:30 -0700

    fixing heroku errors

--- app.js
+++ app.js
@@ -46,13 +46,12 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(mongoSanitize());
 app.use(helmet());
 
-const secret = process.env.SECRET || "thisshouldbeabettersecret!"
+const secret = process.env.SECRET || "thisshouldbeabettersecret!";
 const store = MongoStore.create({
   mongoUrl: dbUrl,
-  secret,
   touchAfter: 24 * 60 * 60,
   crypto: {
-    secret: "thisshouldbeabettersecret!",
+    secret,
   },
 });
 
@@ -156,6 +155,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-app.listen(3000, () => {
-  console.log("Serving on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`);
 });
